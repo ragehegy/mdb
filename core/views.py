da8b2af66372af606e1d902e9ea7b980928f2476
ragehegy <mohamed.rageh.qaid@gmail.com>
2020-11-16 16:11:46 +0200

    ch 1

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView
+)
+from core.models import Movie, Person
+
+class MovieList(ListView):
+    model = Movie
+    paginate_by = 10
+
+class MovieDetail(DetailView):
+    # model = Movie
+    queryset = (Movie.objects.all_with_related_persons())
+
+class PersonDetail(DetailView):
+    queryset = Person.objects.all_with_prefetch_movies()
